c9acdcb029891abdc9c7c44011ab8df8f6f8bac4
Gabe-Thomp <gabe.t.asher@gmail.com>
2025-06-24 10:00:01 -0400

    Adding more complicated personas for profile6,7,8

--- bash_scripts/run_eval_LLM_personas.sh
+++ bash_scripts/run_eval_LLM_personas.sh
@@ -3,13 +3,13 @@
 # === Set parameters here ===
 ENGINE="gpt-4"
 AGENT="questions"         # or edge_cases or pool
-EVAL_CONDITION="at_end" # or per_minute or at_end or per_turn
+EVAL_CONDITION="per_turn" # or per_minute or at_end or per_turn
 POOL_CLUSTERS=-1 
 TASK="website_preferences" # moral_reasoning or email_regex or website_preferences
 # NUM_INTERACTIONS=5
-NUM_INTERACTIONS=2
+NUM_INTERACTIONS=10
 QUESTION_MODES="questions_open"
-NUM_CANDIDATE_QUESTIONS=3
+NUM_CANDIDATE_QUESTIONS=1
 
 # === Call Python script ===
 python run_model_evaluation.py \
